b16418237e88d31f4a0dad985639ae37933fa56e
olivier <thomas>
2016-05-17 13:31:51 +0200

    OTH: Update install

--- install.sh
+++ install.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
+
+# Update
 sudo apt-get upgrade -y
 sudo apt-get update -y
 
+# Install mongo
 sudo apt-get mongodb-server -y
 /etc/init.d/mongodb start
 
+# Setup mongo
+mongo hackathon --eval "db.runCommand({create:'messages'});"
+mongo hackathon --eval "db.messages.createIndex({ id: 1 }, {unique:true});"
+mongo hackathon --eval "db.messages.dropIndex('_id_');" 
+
+# Install node
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 sudo dpkg -i node_latest_armhf.deb
 
+# Install server
 cd ~
 mkdir hackathon
 cd hackathon
-git clone https://github.com/othke/redis_server.git
-cd redis_server
-git checkout -b mongodb origin/mongodb
+https://github.com/othke/iot_server.git
 
+# update npm
+cd iot_server
 npm install
 node cluster.js
